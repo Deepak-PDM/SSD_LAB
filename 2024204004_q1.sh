a90006d6b83f325a50ba3cc905c561b148185407
Deepak-PDM <deepak.yerramsetty@students.iiit.ac.in>
2024-08-21 17:37:38 +0530

    Update 2024204004_q1.sh

--- 2024204004_q1.sh
+++ 2024204004_q1.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-sum=0
-while read line; do
-    IFS='"' line1=($line)
-     IFS=" " line2=($line1[1])
-     echo "${line2[]}"
-     if [[ $line2[1] =~ 'POST' ]]
-     then
-         echo "$line1[1]"
-         fi
-done <access.log
+grep "POST" access.log | grep "404"
 
